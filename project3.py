f0329bde9aae7081ab35264c080b4324cc4949b4
Michael <simpsom@clarkson.edu>
2022-04-20 14:23:59 -0400

    Created main(). Only format changes.

--- project3.py
+++ project3.py
@@ -1,7 +1,28 @@
 import tkinter as tk
 
+
+def main():
+    window = tk.Tk()
+    window.title("Text Editor")
+
+    window.rowconfigure(0,minsize=600,weight=1)
+    window.columnconfigure(1, minsize=900, weight=1)
+
+    txt_edit = tk.Text(window)
+    fr_buttons = tk.Frame(window)
+    #btn_open = tk.Button(fr_buttons, text="Open")
+    btn_save = tk.Button(fr_buttons, text="Get Text", command= lambda: retrieve_input(txt_edit))
+
+    #btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
+    btn_save.grid(row=1, column=0, sticky="ew", padx=10)
+    fr_buttons.grid(row=0, column=0, sticky="ns")
+    txt_edit.grid(row=0, column=1, sticky="nsew")
+
+    window.mainloop()
+
+
 """This function grabs all text in editor upon clicking the get text button and outputs to command line"""
-def retrieve_input():
+def retrieve_input(txt_edit):
     inputValue=txt_edit.get("1.0","end-1c")
     #Following replace lines remove punctuations from the words. Useful for when we do operations on words.
     inputValue = inputValue.replace("!","")
@@ -13,20 +34,5 @@ def retrieve_input():
     print(inputValue)
 
 
-window = tk.Tk()
-window.title("Text Editor")
-
-window.rowconfigure(0,minsize=900,weight=1)
-window.columnconfigure(1, minsize=900, weight=1)
-
-txt_edit = tk.Text(window)
-fr_buttons = tk.Frame(window)
-#btn_open = tk.Button(fr_buttons, text="Open")
-btn_save = tk.Button(fr_buttons, text="Get Text", command=retrieve_input)
-
-#btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
-btn_save.grid(row=1, column=0, sticky="ew", padx=10)
-fr_buttons.grid(row=0, column=0, sticky="ns")
-txt_edit.grid(row=0, column=1, sticky="nsew")
-
-window.mainloop()
+if __name__ == "__main__":
+    main()
